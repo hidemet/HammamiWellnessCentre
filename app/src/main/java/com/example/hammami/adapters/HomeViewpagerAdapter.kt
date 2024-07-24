ec6e60305da309ec811df026f35ddaf080c33c1a
IoSonoRoss <m.rossini026@studenti.unibs.it>
2024-07-24 13:38:24 +0200

    Sistemato allineamento pagina home e introduzione della stampa da path diversi

--- app/src/main/java/com/example/hammami/adapters/HomeViewpagerAdapter.kt
+++ app/src/main/java/com/example/hammami/adapters/HomeViewpagerAdapter.kt
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 package com.example.hammami.adapters
-
-=======
-package com.example.hammami.adapter
->>>>>>> d64d9d4e6b17553dd81a3facfc5f41f029cd4bf1
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
