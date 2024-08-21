e2da4c279808847a6f8f8fc90f54c110f5442584
IoSonoRoss <m.rossini026@studenti.unibs.it>
2024-08-21 21:46:01 +0200

    Continuo creazione codice del servizio in dettaglio

---
+++ app/src/main/java/com/example/hammami/adapters/ReviewsAdapter.kt
@@ -0,0 +1,67 @@
+package com.example.hammami.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.hammami.R
+import com.example.hammami.data.Service
+import com.example.hammami.databinding.ItemHomepageBinding
+import com.example.hammami.databinding.ItemRecensioneBinding
+import com.example.hammami.databinding.ServizioDettaglioBinding
+
+class ReviewsAdapter : RecyclerView.Adapter<ReviewsAdapter.ReviewsViewHolder>() {
+
+    inner class ReviewsViewHolder(private val binding: ItemRecensioneBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(service: Service) {
+            binding.apply {
+                if (service.image != null) {
+                        Glide.with(itemView).load(service.image).into(imageNewRvItem)
+                } else {
+                    // Carica un'immagine placeholder o nascondi l'ImageView
+                    imageNewRvItem.setImageResource(R.drawable.placeholder_image)
+                }
+                tvNewRvItemName.text = service.name
+                tvNewItemRvPrice.text = "${service.price} €"
+            }
+        }
+    }
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Service>() {
+        override fun areItemsTheSame(oldItem: Service, newItem: Service): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Service, newItem: Service): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, diffCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewsViewHolder {
+        return ReviewsViewHolder(
+            ItemRecensioneBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ReviewsViewHolder, position: Int) {
+        val service = differ.currentList[position]
+        holder.bind(service)
+
+        holder.itemView.setOnClickListener {
+            onClick?.invoke(service)
+        }
+    }
+
+    var onClick: ((Service) -> Unit)? = null
+}
--- app/src/main/java/com/example/hammami/fragments/categories/ServiceDetailFragment.kt
+++ app/src/main/java/com/example/hammami/fragments/categories/ServiceDetailFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.hammami.adapters.BestDealsAdapter
+import com.example.hammami.adapters.ReviewsAdapter
 import com.example.hammami.databinding.ServizioDettaglioBinding
 import com.example.hammami.viewmodel.ServizioViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,8 +21,8 @@ class ServiceDetailFragment : Fragment() {
     // private val viewModel: ServizioViewModel by viewModels() // per ora tolgo perchè non serve poi vediamo
     private val args by navArgs<ServiceDetailFragmentArgs>()
 
-    private val viewPagerAdapter by lazy { ViePager2Images() }
-    private val reviewsAdapter by lazy { ColorsAdapter() }
+    // private val viewPagerAdapter by lazy { ViePager2Images() } //per ora tolgo perchè non serve poi vediamo
+    private val reviewsAdapter by lazy { ReviewsAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -35,16 +37,27 @@ class ServiceDetailFragment : Fragment() {
         val service = args.service
 
         setupReviewsRV()
-        setupViewPager()
+        //setupViewPager()
 
         binding.apply {
             tvTitolo.text = service.name
             tvContenutoDescrizione.text = service.description
             tvContenutoDurata.text = "${service.length} h"
             tvContenutoPrezzo.text = "${service.price} €"
+
+            if(service.reviews.isNullOrEmpty()) {
+                tvTitoloRecensioni.visibility = View.GONE
+                rvRecensioni.visibility = View.GONE
+            }
+
+            if(service.benefits.isNullOrEmpty()) {
+                tvTitoloBenefici.visibility = View.GONE
+                tvContenutoBenefici.visibility = View.GONE
+            } else {
+                tvContenutoBenefici.text = service.benefits
+            }
         }
 
-        viewPagerAdapter.differ.submitList(service.images)
         service.reviews?.let { reviewsAdapter.differ.submitList(it) }
 
 
@@ -62,13 +75,8 @@ class ServiceDetailFragment : Fragment() {
          */
     }
 
-    private fun setupViewPager() {
-        binding.apply {
-            viewPagerProductImages.adapter = viewPagerAdapter
-        }
-    }
-
     private fun setupReviewsRV() {
+        reviewsAdapter = ReviewsAdapter()
         binding.rvRecensioni.apply {
             adapter = reviewsAdapter
             layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
