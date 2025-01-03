9d1cc5c37be346e8e224fda149731d7aec134aed
IoSonoRoss <m.rossini026@studenti.unibs.it>
2025-01-03 11:33:16 +0100

    Ho tolto i commenti in NewAppointmentsFragment.kt

--- app/src/main/java/com/example/hammami/presentation/ui/features/appointments/NewAppointmentsFragment.kt
+++ app/src/main/java/com/example/hammami/presentation/ui/features/appointments/NewAppointmentsFragment.kt
@@ -16,7 +16,7 @@ import com.example.hammami.databinding.FragmentBenessereBinding
 import com.example.hammami.databinding.FragmentNewAppointmentsBinding
 import com.example.hammami.databinding.FragmentPastAppointmentsBinding
 import com.example.hammami.presentation.ui.activities.UserProfileViewModel
-//import com.example.hammami.presentation.ui.adapters.AppointmentAdapter
+import com.example.hammami.presentation.ui.adapters.AppointmentAdapter
 import com.example.hammami.presentation.ui.adapters.BenessereAdapter
 import com.example.hammami.presentation.ui.features.BaseFragment
 import com.example.hammami.presentation.ui.features.service.BenessereFragment
@@ -25,70 +25,70 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
-//@AndroidEntryPoint
-//class NewAppointmentsFragment : BaseFragment(){
-//
-//    private lateinit var appointmentAdapter: AppointmentAdapter
-//
-//    private var _binding: FragmentNewAppointmentsBinding? = null
-//    private val binding get() = _binding!!
-//
-//    private val viewModel: AppointmentsViewModel by viewModels()
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-//    ): View {
-//        _binding = FragmentNewAppointmentsBinding.inflate(inflater, container, false)
-//        return binding.root
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        viewModel.loadNewAppointmentsData(viewModel.userEmail!!)
-//        Log.e("NewAppointmentsFragment", "email: ${viewModel.userEmail}")
-//        //Log.e("NewAppointmentsFragment", "emailUiState: ${viewModel.uiState.value.user?.email}")
-//        setupUI()
-//        observeFlows()
-//    }
-//
-//    override fun setupUI() {
-//        //setupAppBar()
-//        setupRecyclerView()
-//    }
-//
-//    override fun observeFlows() {
-//        viewLifecycleOwner.lifecycleScope.launch {
-//            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-//                launch { observeAppointments() }
-//                //launch { observeEvents() }
-//            }
-//        }
-//    }
-//
-//    private suspend fun observeAppointments() {
-//        viewModel.newAppointments.collectLatest { state ->
-//            //updateUI(state)
-//            appointmentAdapter.submitList(state)
-//        }
-//    }
-//
-//
-//    private fun handleEvent(event: AppointmentsViewModel.UiEvent) {
-//        when (event) {
-//            is AppointmentsViewModel.UiEvent.ShowMessage -> showSnackbar(event.message)
-//            is AppointmentsViewModel.UiEvent.ShowError -> showSnackbar(event.message)
-//            else -> {}
-//        }
-//    }
-//
-//    private fun setupRecyclerView() {
-//        appointmentAdapter = AppointmentAdapter()
-//        binding.rvNewAppointments.apply{
-//            layoutManager =
-//                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-//            adapter = appointmentAdapter
-//        }
-//    }
+@AndroidEntryPoint
+class NewAppointmentsFragment : BaseFragment(){
+
+    private lateinit var appointmentAdapter: AppointmentAdapter
+
+    private var _binding: FragmentNewAppointmentsBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: AppointmentsViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNewAppointmentsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.loadNewAppointmentsData(viewModel.userEmail!!)
+        Log.e("NewAppointmentsFragment", "email: ${viewModel.userEmail}")
+        Log.e("NewAppointmentsFragment", "emailUiState: ${viewModel.uiState.value.user?.email}")
+        setupUI()
+        observeFlows()
+    }
+
+    override fun setupUI() {
+        setupAppBar()
+        setupRecyclerView()
+    }
+
+    override fun observeFlows() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch { observeAppointments() }
+                launch { observeEvents() }
+            }
+        }
+    }
+
+    private suspend fun observeAppointments() {
+        viewModel.newAppointments.collectLatest { state ->
+            updateUI(state)
+            appointmentAdapter.submitList(state)
+        }
+    }
+
+
+    private fun handleEvent(event: AppointmentsViewModel.UiEvent) {
+        when (event) {
+            is AppointmentsViewModel.UiEvent.ShowMessage -> showSnackbar(event.message)
+            is AppointmentsViewModel.UiEvent.ShowError -> showSnackbar(event.message)
+            else -> {}
+        }
+    }
+
+    private fun setupRecyclerView() {
+        appointmentAdapter = AppointmentAdapter()
+        binding.rvNewAppointments.apply{
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = appointmentAdapter
+        }
+    }
 
     /*
     override fun onResume() {
@@ -109,7 +109,7 @@ import kotlinx.coroutines.launch
 
      */
 
-//}
+}
 
 
 
